f8a301f0ac69811b489affa1e05bd71a1b23b5bd
lyllesubesa <subesalylle909@gmail.com>
2024-10-11 22:59:23 +0800

    DisplaViaforloop

---
+++ src/DisplayViaforloop/DisplayViaforLoop.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DisplayViaforLoop;
+
+/**
+ *
+ * @author HP
+ */
+public class DisplayViaforLoop {
+     public static void main(String[] args) {
+        
+        //Arrays 
+        String[] names = {"Lylle", "Elay", "Lylley", "LYLY"};
+        String[] year = {"First year", "Second Year", "Third year", "Fourth year"};
+        
+        //Foor Loops to Print All the values of the variable year and names
+        for(int i = 0; i < year.length; i++) {
+             //Output
+             System.out.println(names[i] + " " + year[i]);
+         }
+         
+     }
+}
---
+++ src/MultidimensionalArray/MultidimensionalArray.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package MultidimensionalArray;
+
+/**
+ *
+ * @author HP
+ */
+public class MultidimensionalArray {
+     public static void main(String[] args) {
+         
+         //2D Array of Program and Year
+         String[][] programAndyear ={
+             {"BSIT", "First year"},
+             {"BSBA", "Second year"},   
+             {"BSED", "Third year"}
+         };
+         //Process using For Loops
+         for(int i = 0; i < programAndyear.length; i++) {
+             for(int j = 0; j < programAndyear[i].length; j++ ) {
+                 System.out.print(programAndyear[i][j] + " ");
+             }
+             //Output
+             System.out.println();
+         }
+     }
+}
