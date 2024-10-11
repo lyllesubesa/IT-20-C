6010e6101ce7a713f36d481e589e7fa5d1c48c73
lyllesubesa <subesalylle909@gmail.com>
2024-10-11 23:50:45 +0800

    SimpleImplementation

---
+++ src/SimpleImplementation/SimpleImplementation.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SimpleImplementation;
+
+/**
+ *
+ * @author HP
+ */
+public class SimpleImplementation {
+    public static void main(String[] args) {
+    
+    //Array 
+    int[] num = {1, 2, 3, 4, 5};    
+    //Output and Process by calling its index to print values
+    System.out.println(num[0]);
+    System.out.println(num[1]);
+    System.out.println(num[2]);
+    System.out.println(num[3]);
+    System.out.println(num[4]);
+    }
+    
+}
